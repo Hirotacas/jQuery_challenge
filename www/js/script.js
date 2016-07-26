f0e82482cd2073bc7f0fae4375b660acf4c6e29c
hirotaca <developer.hiro.east9.hey.world@gmail.com>
2016-07-26 14:45:13 +0900

    fix: メニュー切り替えアニメーション。アニメーション中は、リクエストを受け付けない。

--- www/js/script.js
+++ www/js/script.js
@@ -1,9 +1,13 @@
 $(function () {
+
+    /* ボタンhover時 main pageを表示*/
     $('a').hover(function () {
         $('#main').load($(this).attr('href'))
     });
+
+    /* メニュー切り替え */
     $('#more').on('click', function () {
-        $('div.more_menu').slideToggle();
-        $('div.main_menu').slideToggle();
+        $('div.more_menu:not(:animated)').slideToggle();
+        $('div.main_menu:not(:animated)').slideToggle();
     });
 });
